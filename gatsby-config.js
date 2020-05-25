ad800347f9f6d753e1dea21b2d82c4f4a4d573fc
Noah Johnson <jnoahjohnson@gmail.com>
2020-05-24 21:51:27 -0600

    Updates:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,22 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
+
+const path = require(`path`);
 module.exports = {
   siteMetadata: {
     title: `2020 Summer Health Challenge`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 import { Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 const Header = () => {
-  console.log("something");
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fixed(width: 190) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
 
   return (
-    <div className="w-full items-center justify-between bg-red-700 ">
+    <div className="w-full items-center justify-between bg-gray-100 ">
       <nav className="w-11/12 mx-auto flex items-center justify-between p-6 bg-transparent max-w-screen-lg">
-        <div className="items-left w-1/2 text-white mr-6 text-left">
-          <span className="font-semibold text-xl">2020 Health Challenge</span>
-        </div>
+        {/* <div className="items-left w-1/2 text-white mr-6 text-left"> */}
+        {/* <span className="font-semibold text-xl">2020 Health Challenge</span> */}
+
+        {/* </div> */}
+        <Img fixed={data.file.childImageSharp.fixed} />
         <div className="w-1/2 mt-1 text-md flex flex-row-reverse">
           <Link to="/app/profile">
-            <a className="block text-white hover:text-grey-500 mr-4">
+            <a className="block text-black hover:text-grey-500 mr-4">
               Leaderboard
             </a>
           </Link>
           <Link to="/">
-            <a className="block text-white hover:text-grey-500 mr-4">Home</a>
+            <a className="block text-black hover:text-grey-500 mr-4">Home</a>
           </Link>
         </div>
       </nav>
